1eb0ec87e9c9aedb4eb46c5295ec4d929226fa85
Rodrigo Costa <rodrigocosta@outlook.it>
2023-02-21 22:00:08 -0400

    refatorando view e controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,40 +5,40 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUpdateUserFormRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+
 class UserController extends Controller
 {
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
     public function index(Request $request)
     {
-       
-        // $users = User::get();
+
+        // $users = $this->model->get();
         #$users = User::where('name','LIKE',"%{$request->search}%")->get()->toSql();
-       
-        $search = $request->search;
 
-        $users = User::where(function ($query) use ($search){
-            if($search){
-                $query->where('email',$search);
-                $query->orWhere('name','LIKE',"%{$search}%");
-            }
-        })->get();
-
+        $users = $this->model->getUsers(search: $request->search ?? '');
 
         #também poderia ser compact('users')
-        return view('users.index',[
+        return view('users.index', [
             'users' => $users
         ]);
     }
 
     public function show($id)
     {
-        #$user = User::find($id);
-        
-        $user = User::where('id','=',$id)->first();
+        #$user = $this->model->find($id);
 
-        if(!$user){
+        $user = $this->model->where('id', '=', $id)->first();
+
+        if (!$user) {
             return redirect()->route('users.index');
-        }else{
-            return view('users.show',compact('user'));
+        } else {
+            return view('users.show', compact('user'));
         }
     }
 
@@ -48,62 +48,60 @@ class UserController extends Controller
     }
 
     public function store(StoreUpdateUserFormRequest $request)
-    {   
+    {
         #$request->all()
         #$request->only(['name', 'email','password'])
-        
-    //    $user = new User;
-    //    $user->name = $request->name;
-    //    $user->email = $request->email;
-    //    $user->senha = bcrypt($request->password);
-    //    $user->save();
+
+        //    $user = new User;
+        //    $user->name = $request->name;
+        //    $user->email = $request->email;
+        //    $user->senha = bcrypt($request->password);
+        //    $user->save();
 
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
 
-        $user = User::create($data);
+        $user = $this->model->create($data);
         #return redirect()->route('users.show',['id' => $user->id]);
         return redirect()->route('users.index');
-
     }
 
     public function edit($id)
     {
-        $user = User::find($id);
-        
-        if($user){
-           return view('users.edit',compact('user'));
-        }else{
+        $user = $this->model->find($id);
+
+        if ($user) {
+            return view('users.edit', compact('user'));
+        } else {
             return redirect()->route('users.index');
         }
     }
 
     public function update(StoreUpdateUserFormRequest $request, $id)
     {
-        $user = User::find($id);
-        $data = $request->only(['name','email']);
+        $user = $this->model->find($id);
+        $data = $request->only(['name', 'email']);
 
-        if($request->password)
+        if ($request->password)
             $data['password'] = bcrypt(($request->password));
 
-        if($user){
-           $user->update($data);
-           return redirect()->route('users.index');
-        }else{
+        if ($user) {
+            $user->update($data);
+            return redirect()->route('users.index');
+        } else {
             return redirect()->route('users.index');
         }
     }
 
     public function delete($id)
     {
-         $user = User::find($id);
-       
-         if($user){
+        $user = $this->model->find($id);
+
+        if ($user) {
             $user->delete();
             return redirect()->route('users.index');
-         }else{
-             return redirect()->route('users.index');
-         }
-     }
-
+        } else {
+            return redirect()->route('users.index');
+        }
+    }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
-@section('title','criar um novo usuário')
+@section('title', 'criar um novo usuário')
 @section('content')
-<h1>Cadastrar novo usuário</h1>
+    <h1>Cadastrar novo usuário</h1>
 
-@if($errors->any())
-    <ul class="errors">
-        @foreach($errors->all() as $error)
-            <li class="error">{{$error}}</li>
-        @endforeach
-    </ul>
-@endif
-
-<form action="{{route('users.store')}}" method="post">
-    @csrf()
+    @include('includes.validations-form')
+    
+    <form action="{{ route('users.store') }}" method="post">
+        {{-- @csrf()
     <input type="text" name="name" placeholder="Nome:" value="{{old('name')}}">
     <input type="email" name="email" placeholder="E-mail" value="{{old('email')}}">
     <input type="password" name="password" placeholder="senha" value="{{old('password')}}">
     <button type="submit">
         Enviar
-    </button>
-</form>
+    </button> --}}
+        @include('users._partials.form')
+    </form>
 @endsection
