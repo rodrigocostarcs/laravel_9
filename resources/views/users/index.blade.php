ca50540f5df867760e876f084784b6a86344767a
Rodrigo Costa <rodrigocosta@outlook.it>
2023-03-05 14:48:38 -0400

    edição de imagens

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.app') <!--Pega de layouts app para usar o template de lá-->
+@extends('layouts.app')
 
-@section('title','listagem de usuários')
-    
-@section('content') <!--Para essa seção definida no layouts.app vou por este conteúdo.--> 
-<h3 class="text-2xl font-semibold leading-tigh py-2">
-    Listagem dos Usuários | <a href="{{route('users.create')}}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
-</h3>
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
+<h1 class="text-2xl font-semibold leading-tigh py-2">
+    Listagem dos usuários
+    <a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
+</h1>
 
 <form action="{{ route('users.index') }}" method="get" class="py-5">
     <input type="text" name="search" placeholder="Pesquisar" class="md:w-1/6 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
@@ -33,6 +34,11 @@
           <th
             class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
           >
+            Detalhes
+          </th>
+          <th
+            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+          >
             Comentários
           </th>
         </tr>
@@ -40,7 +46,14 @@
       <tbody>
     @foreach ($users as $user)
         <tr>
-            
+            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                @if ($user->image)
+                    <img src="{{ url("storage/{$user->image}") }}" alt="{{ $user->name }}" class="object-cover w-20">
+                @else
+                    <img src="{{ url("images/favicon.ico") }}" alt="{{ $user->name }}" class="object-cover w-20">
+                @endif
+                {{ $user->name }}
+            </td>
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">{{ $user->email }}</td>
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                 <a href="{{ route('users.edit', $user->id) }}" class="bg-green-200 rounded-full py-2 px-6">Editar</a>
@@ -49,17 +62,17 @@
                 <a href="{{ route('users.show', $user->id) }}" class="bg-orange-200 rounded-full py-2 px-6">Detalhes</a>
             </td>
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-              <a href="{{ route('comments.index', $user->id) }}" class="bg-blue-200 rounded-full py-2 px-6">Anotações ({{$user->comments->count()}})</a>
-           </td>
+                <a href="{{ route('comments.index', $user->id) }}" class="bg-blue-200 rounded-full py-2 px-6">Anotações ({{ $user->comments->count() }})</a>
+            </td>
         </tr>
     @endforeach
     </tbody>
 </table>
 
 <div class="py-4">
-  {{$users->appends([
-    'search' => request()->get('search','')
-  ])->links()}}
+    {{ $users->appends([
+        'search' => request()->get('search', '')
+    ])->links() }}
 </div>
 
 @endsection
